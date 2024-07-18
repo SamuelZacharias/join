f404163c383e6cd1831e067349a1942eedf495f3
NaviPlau <naviplau@outlook.com>
2024-07-18 14:47:46 +0200

    added toggling visibility

--- js/register.js
+++ js/register.js
@@ -124,3 +124,14 @@ function loadRegisterData() {
   }
   return [];
 }
+
+function showPassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+    document.getElementById('visibility').src="assets/img/png/visibility.png"
+  } else {
+    x.type = "password";
+    document.getElementById('visibility').src="assets/img/png/visibility_off.png"
+  }
+}
