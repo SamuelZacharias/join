28838cfedb48447a2d565bb9f68a406cc44d5c75
Alexej Kaljuzhin <alexej.kaljuzhin@googlemail.com>
2024-07-19 15:57:01 -0400

    dialog fenster bei contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,5 +1,14 @@
-function openDialog(event){
-    event.stopPropagation();
+function openDialog(){
+    // event.stopPropagation();
   const dialogContainer = document.getElementById("dialog-contacts");
   dialogContainer.open = true;
+  dialogContainer.classList.add("d-flex");
+  document.getElementById("grey-background").classList.remove("hidden");
+}
+
+function closeDialog(){
+    const dialogContainer = document.getElementById("dialog-contacts");
+    dialogContainer.open = false;
+    dialogContainer.classList.remove("d-flex");
+    document.getElementById("grey-background").classList.add("hidden");
 }
