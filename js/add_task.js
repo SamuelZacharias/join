561435313efb3ccf9a9e14ed185008f6f9bca40a
NaviPlau <naviplau@outlook.com>
2024-07-20 14:20:15 +0200

    add-task-js

--- js/add_task.js
+++ js/add_task.js
@@ -1,12 +1,14 @@
-var expanded = false;
+function openDropDown(){
+  let dropdown = document.getElementById('dropdown');
+  dropdown.innerHTML = `
+  <div style="display:flex; flex-direction:column;">
+    <div >
+      <span>Select task category</span>
+      <img class="dropUpImg" src="assets/img/png/arrow_drop_down (1).png" alt="">
+    </div>
+    <span>Technical Task</span>
+    <span>User Story</span>
+  </div>
+  `;
 
-function showCheckboxes() {
-  var checkboxes = document.getElementById("checkboxes");
-  if (!expanded) {
-    checkboxes.style.display = "block";
-    expanded = true;
-  } else {
-    checkboxes.style.display = "none";
-    expanded = false;
-  }
 }
