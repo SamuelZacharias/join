efc0c34842f1e83dca71d332ea54a5fdae0fd2ba
Alexej Kaljuzhin <alexej.kaljuzhin@googlemail.com>
2024-07-22 14:53:23 -0400

    add contact fertig

--- js/contacts.js
+++ js/contacts.js
@@ -1,14 +1,23 @@
-function openDialog(){
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function openDialog(){
     // event.stopPropagation();
   const dialogContainer = document.getElementById("dialog-contacts");
   dialogContainer.open = true;
   dialogContainer.classList.add("d-flex");
+  await sleep(10);
+  dialogContainer.classList.add("dialog-open")
   document.getElementById("grey-background").classList.remove("hidden");
 }
 
-function closeDialog(){
+async function closeDialog(){
     const dialogContainer = document.getElementById("dialog-contacts");
-    dialogContainer.open = false;
-    dialogContainer.classList.remove("d-flex");
+    dialogContainer.classList.remove("dialog-open")
     document.getElementById("grey-background").classList.add("hidden");
+    await sleep(300);
+    dialogContainer.classList.remove("d-flex");
+    dialogContainer.open = false;;
+    
 }
