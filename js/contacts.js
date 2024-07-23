ba884a40a349d684e17876746267dfea2f9c1f73
Alexej Kaljuzhin <alexej.kaljuzhin@googlemail.com>
2024-07-23 11:26:15 -0400

    contacts hmtl css fertig fehlt JS

--- js/contacts.js
+++ js/contacts.js
@@ -1,23 +1,40 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function openDialog(){
-    // event.stopPropagation();
+async function openDialog() {
+  // event.stopPropagation();
   const dialogContainer = document.getElementById("dialog-contacts");
   dialogContainer.open = true;
   dialogContainer.classList.add("d-flex");
   await sleep(10);
-  dialogContainer.classList.add("dialog-open")
+  dialogContainer.classList.add("dialog-open");
   document.getElementById("grey-background").classList.remove("hidden");
 }
 
-async function closeDialog(){
-    const dialogContainer = document.getElementById("dialog-contacts");
-    dialogContainer.classList.remove("dialog-open")
-    document.getElementById("grey-background").classList.add("hidden");
-    await sleep(300);
-    dialogContainer.classList.remove("d-flex");
-    dialogContainer.open = false;;
-    
+async function closeDialog() {
+  const dialogContainer = document.getElementById("dialog-contacts");
+  dialogContainer.classList.remove("dialog-open");
+  document.getElementById("grey-background").classList.add("hidden");
+  await sleep(300);
+  dialogContainer.classList.remove("d-flex");
+  dialogContainer.open = false;
+}
+
+async function openDialogEdit(){
+  const dialogContainer = document.getElementById("dialog-edit");
+  dialogContainer.open = true;
+  dialogContainer.classList.add("d-flex");
+  await sleep(10);
+  dialogContainer.classList.add("dialog-open");
+  document.getElementById("grey-background").classList.remove("hidden");
+}
+
+async function closeDialogEdit() {
+  const dialogContainer = document.getElementById("dialog-edit");
+  dialogContainer.classList.remove("dialog-open");
+  document.getElementById("grey-background").classList.add("hidden");
+  await sleep(300);
+  dialogContainer.classList.remove("d-flex");
+  dialogContainer.open = false;
 }
