29e3e79bf17ac4bcb9e31bad1bee9323a2254c96
Samuel Zacharias <samuel.zacharias@web.de>
2024-07-27 13:16:34 +0200

    update summary incl. js

--- js/summary.js
+++ js/summary.js
@@ -11,3 +11,39 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log('No logged-in user name found');
   }
 });
+
+function getGreeting() {
+    const now = new Date(); // Erstellen Sie ein neues Date-Objekt für die aktuelle Zeit
+    const hours = now.getHours(); // Stunden aus dem Date-Objekt holen
+
+    let greeting;
+
+    if (hours < 12) {
+        greeting = "Good morning"; // Begrüßung für die Zeit von 00:00 bis 11:59
+    } else if (hours < 18) {
+        greeting = "Good afternoon"; // Begrüßung für die Zeit von 12:00 bis 17:59
+    } else {
+        greeting = "Good evening"; // Begrüßung für die Zeit von 18:00 bis 23:59
+    }
+
+    return greeting;
+}
+
+function generateGreetingHTML() {
+    const time = getGreeting(); // Aktuelle Begrüßung ermitteln
+    // HTML-String mit der Begrüßung als Variable
+    const htmlContent = `<div>
+        <h1>${time}</h1>
+    </div>`;
+    return htmlContent;
+}
+
+console.log(generateGreetingHTML()); // Ausgabe des HTML-Inhalts
+
+function updateGreeting() {
+    const greeting = getGreeting();
+    document.querySelector('.greetBox1').textContent = greeting + ',';
+}
+
+// Aktualisieren der Begrüßung, sobald die Seite geladen ist
+window.onload = updateGreeting;
