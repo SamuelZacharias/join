eb1c48c76283b527f34dcf6a6b236d21e72ba53b
NaviPlau <naviplau@outlook.com>
2024-08-04 10:22:54 +0200

    update-board-attTask

--- js/board.js
+++ js/board.js
@@ -188,8 +188,8 @@ function renderAddTaskBoardHtml(addTaskContainer){
                     <span class="spanCategory" >Select Contacts to assign</span>
                     <img class="dropDownImg" id="dropDownContactsImg" src="assets/img/png/arrow_drop_down (1).png" alt="">
                   </div>
-                  <div id="contacts" class="d-none"></div>
-                  <div id="assignedContacts"></div>
+                  <div id="contacts" class=" contacts d-none"></div>
+                  <div id="assignedContacts" class="assignedContacts"></div>
                 </div>
               </div>
               <div class="separator"></div>
@@ -220,24 +220,24 @@ function renderAddTaskBoardHtml(addTaskContainer){
                 <div id="categories"></div>
                 <div class="eachInput">
                   <span>Subtasks</span>
-                  <div id="subtaskContainer">
+                  <div id="subtaskContainerAddTask">
                     <p>
                       <input type="text" name="" placeholder="Add new subtask" readonly onclick="writeSubtaskAddTask()" />
                       <img src="assets/img/png/Subtasks icons11.png"  onclick="writeSubtaskAddTask()"alt="" />
                     </p>
                   </div>
-                  <ul id="newSubtasks" style="display: flex;"></ul>
+                  <ul id="newSubtasksAddTask" style="display: flex;"></ul>
                 </div>
               </div>
             </form>
             <section class="buttonsSection d-flex">
               <span><b style="color: red">*</b> This field is Required</span>
               <div class="buttonArea">
-                <button id="clearButton" class="buttonCenter clear">
+                <button id="clearButton" onclick="clearForm()" class="buttonCenter clear">
                   Clear
                   <img src="assets/img/png/iconoir_cancel.png" alt="" />
                 </button>
-                <button type="submit" class="buttonCenter createButton">
+                <button type="submit" onlick="handleCreateButtonClick()" class="buttonCenter createButton">
                   Create Task <img src="assets/img/png/check.png" alt="" />
                 </button>
               </div>
@@ -464,3 +464,389 @@ document.addEventListener('click', function(event) {
     document.getElementById('dropDownContactsImg').classList.remove('dropUpImg')
     }
   });
+
+  let addTaskBoardInfos = [];
+
+function writeSubtaskAddTask() {
+  let subtaskArea = document.getElementById('subtaskContainerAddTask');
+  subtaskArea.innerHTML = `
+      <div class="addSubtask">
+          <input type="text" name="" autofocus id="subtaskInput" minlength="3" required placeholder="Enter subtask"/>
+          <div class="d-flex">
+              <img src="assets/img/png/subtaskX.png" onclick="resetSubtask()" alt="" />
+              <img src="assets/img/png/subtaskDone.png" onclick="addSubtaaskBoard();" alt="" />
+          </div>
+      </div>
+  `;
+
+  // Get the newly created input field
+  let inputField = document.getElementById('subtaskInput');
+
+  // Add event listener for focusout event
+  inputField.addEventListener('focusout', function() {
+      if (!this.value.trim()) {
+          resetSubtask();
+      }
+  });
+
+  // Add event listener for keydown event to detect Enter key
+  inputField.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+          event.preventDefault(); // Prevent the default action if necessary
+          addSubtaaskBoard(); // Call the addSubtask function
+      }
+  });
+
+  // Ensure the input gets focus
+  inputField.focus();
+}
+
+function addSubtaaskBoard(){
+  let subtaskInput = document.getElementById('subtaskInput');
+  let subtaskInfo = subtaskInput.value;
+
+  if (subtaskInfo.length < 3) {
+    subtaskInput.value = ''; 
+    subtaskInput.placeholder = 'Min 3 characters needed'; 
+    subtaskInput.style.borderColor = 'red'; 
+    subtaskInput.classList.add('error-placeholder'); 
+    return; 
+  } else {
+    subtaskInput.placeholder = 'Enter subtask'; 
+    subtaskInput.style.borderColor = ''; 
+    subtaskInput.classList.remove('error-placeholder'); 
+  }
+
+  addTaskBoardInfos.push(subtaskInfo);
+  showSubtasksAddTask();
+  resetSubtask();
+}
+
+function resetSubtask() {
+  document.getElementById('subtaskContainerAddTask').innerHTML = `
+    <p>
+      <input type="text" autofocus name="" placeholder="Add new subtask" readonly onclick="writeSubtaskAddTask()" />
+      <img src="assets/img/png/Subtasks icons11.png" alt="" />
+    </p>
+  `;
+}
+
+function showSubtasksAddTask() {
+  let newSubtask = document.getElementById('newSubtasksAddTask');
+  newSubtask.innerHTML = '';
+  for (let s = 0; s < addTaskBoardInfos.length; s++) {
+    newSubtask.innerHTML += `
+      <li onmouseenter="showActions(this)" onmouseleave="hideActionsAddTask(this)">
+        <div class="subtask-item">
+          <div class="subtask-content">
+            <span class="custom-bullet">•</span>
+              <div style="width:100%" onclick="editSubtaskAddTask(${s})">${addTaskBoardInfos[s]}</div>
+          </div>
+          <div class="subtaskIconsAddTask d-none">
+            <img src="assets/img/png/editSubtask.png" onclick="editSubtaskAddTask(${s})" alt="" />
+            <div class="vertical-line"></div>
+            <img src="assets/img/png/delete.png" onclick="deleteSubtaskAddTask(${s})" alt="" />
+          </div>
+        </div>
+      </li>
+    `;
+  }
+}
+
+function editSubtaskAddTask(index) {
+  let newSubtask = document.getElementById('newSubtasksAddTask');
+  
+  for (let s = 0; s < addTaskBoardInfos.length; s++) {
+    if (s === index) {
+      newSubtask.innerHTML = `
+        <div class="addSubtask" >
+          <input type="text" id="editSubtaskInputAddTask" value="${addTaskBoardInfos[s]}" minlength="3" required />
+          <div class="d-flex">
+            <img src="assets/img/png/subtaskDone.png" onclick="saveSubtaskAddTask(${s})" alt="" />
+            <img src="assets/img/png/delete.png" onclick="deleteSubtaskAddTask(${s})" alt="" />
+          </div>
+        </div>
+      `;
+    } else {
+      newSubtask.innerHTML = `
+        <div class="addSubtask">
+          <div style="width:100%" onclick="editSubtaskAddTask(${s})"  >
+            ${addTaskBoardInfos[s]}
+          </div>
+          <div class="d-flex">
+            <img src="assets/img/png/subtaskDone.png" onclick="showSubtasksAddTask()" alt="" />
+            <img src="assets/img/png/delete.png" onclick="deleteSubtaskAddTask(${s})" alt="" />
+          </div>
+        </div>
+      `;
+    }
+  }
+  let inputField = document.getElementById('editSubtaskInputAddTask');
+    if (inputField) {
+        inputField.addEventListener('keydown', function(event) {
+            if (event.key === 'Enter') {
+                event.preventDefault(); // Prevent the default action if necessary
+                saveSubtaskAddTask(index); // Call the saveSubtask function
+            }
+        });
+        inputField.focus(); 
+};
+}
+
+function saveSubtaskAddTask(index) {
+  let editInput = document.getElementById('editSubtaskInputAddTask');
+  let editedSubtask = editInput.value;
+
+  if (editedSubtask.length < 3) {
+    editInput.value = ''; 
+    editInput.placeholder = 'Min 3 characters needed'; 
+    editInput.style.borderColor = 'red'; 
+    editInput.classList.add('error-placeholder'); 
+    return; 
+  } else {
+    editInput.placeholder = ''; 
+    editInput.style.borderColor = ''; 
+    editInput.classList.remove('error-placeholder'); 
+  }
+
+  addTaskBoardInfos[index] = editedSubtask;
+  showSubtasksAddTask()
+}
+
+function deleteSubtaskAddTask(index) {
+  addTaskBoardInfos.splice(index, 1);
+  showSubtasksAddTask()
+}
+
+function showActions(element) {
+  let actions = element.querySelector('.subtaskIconsAddTask');
+  if (actions) {
+    actions.classList.remove('d-none');
+  }
+}
+
+function hideActionsAddTask(element) {
+  let actions = element.querySelector('.subtaskIconsAddTask');
+  if (actions) {
+    actions.classList.add('d-none');
+  }
+}
+
+function setMinDate() {
+  const dateInput = document.getElementById('dateInput');
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const dd = String(today.getDate()).padStart(2, '0');
+  const todayDate = `${yyyy}-${mm}-${dd}`;
+
+  dateInput.min = todayDate;
+}
+
+
+
+function clearForm() {
+  event.preventDefault();
+  const form = document.getElementById('taskForm');
+  form.reset();
+  document.getElementById('dropdownCategory').innerHTML = `
+    <span class="spanCategory">Select task category</span>
+    <img class="dropDownImg" id="dropDownImg" src="assets/img/png/arrow_drop_down (1).png" alt="">
+  `;
+  document.getElementById('assignedContacts').innerHTML = ``;
+  document.getElementById('newSubtasksAddTask').innerHTML = ``;
+  choosenCategory = false;
+  clickCount = 0;
+  selectedContacts = [];
+  addTaskBoardInfos = [];
+  handleClick(2); 
+  document.querySelectorAll('.inputContainer').forEach(p => {
+    p.classList.remove('invalid');
+  });
+  document.getElementById('dropdownCategory').classList.remove('invalid');
+}
+
+function submitForm(event) {
+  if (!validateForm()) {
+    event.preventDefault();
+  } else {
+    document.getElementById('taskForm').submit();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const dropdownCategory = document.getElementById('dropdownCategory');
+  if (dropdownCategory) {
+    dropdownCategory.addEventListener('click', function() {
+      if (choosenCategory) {
+        dropdownCategory.classList.remove('invalid');
+      }
+    });
+  }
+});
+
+
+function validateForm() {
+  const form = document.getElementById('taskForm');
+  const inputs = form.querySelectorAll('input, textarea, select');
+  let valid = true;
+
+  
+  document.querySelectorAll('.inputContainer').forEach(p => {
+    p.classList.remove('invalid');
+  });
+  document.getElementById('dropdownCategory').classList.remove('invalid');
+
+  
+  inputs.forEach(input => {
+    const parentP = input.closest('.inputContainer');
+    if (input.required && !input.value.trim()) {
+      valid = false;
+      if (parentP) {
+        parentP.classList.add('invalid');
+      }
+    }
+
+    
+    if (input.type === 'date') {
+      const selectedDate = new Date(input.value);
+      const today = new Date();
+      
+      today.setHours(0, 0, 0, 0);
+      if (selectedDate < today) {
+        valid = false;
+        if (parentP) {
+          parentP.classList.add('invalid');
+        }
+      }
+    }
+  });
+
+ 
+  if (!choosenCategory) {
+    valid = false;
+    document.getElementById('dropdownCategory').classList.add('invalid');
+  }
+  
+  return valid;
+  
+}
+
+async function handleCreateButtonClick() {
+  event.preventDefault(); 
+
+  if (validateForm()) {
+    collectData(); 
+
+    try {
+      await sendTaskDataToFirebase(); 
+    } catch (error) {
+      console.error('Failed to send task data to Firebase:', error); 
+    }
+  } 
+}
+const BASE_TASKS_URL = 'https://join-40dd0-default-rtdb.europe-west1.firebasedatabase.app/tasks/';
+
+function collectData() {
+  const form = document.getElementById('taskForm');
+  if (!form) {
+    return;
+  }
+
+  const title = form.querySelector('input[type="text"]').value;
+  const description = form.querySelector('textarea').value;
+  const dueDate = form.querySelector('input[type="date"]').value;
+  const categoryElement = document.getElementById('dropdownCategory').querySelector('span').innerText;
+
+  const assignedContacts = selectedContacts;
+  
+  // Set 'completed' to false for each subtask
+  const subtasks = addTaskBoardInfos.map(subtask => ({ title: subtask, completed: false }));
+
+  let priority = '';
+  switch (activeButton) {
+    case 1:
+      priority = 'Urgent';
+      break;
+    case 2:
+      priority = 'Medium';
+      break;
+    case 3:
+      priority = 'Low';
+      break;
+    default:
+      console.error("Invalid activeButton value");
+      return;
+  }
+
+  return {
+    title: title,
+    description: description,
+    dueDate: dueDate,
+    priority: priority,
+    category: categoryElement,
+    assignedContacts: assignedContacts,
+    subtasks: subtasks,
+    column: 'toDo',
+  };
+}
+
+async function sendTaskDataToFirebase() {
+  try {
+    await initializeTasksNode(); // Ensure the 'tasks' node exists
+
+    // Fetch existing tasks to determine the next index
+    const response = await fetch(`${BASE_TASKS_URL}.json`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const existingTasks = await response.json();
+    let tasksArray = existingTasks ? Object.keys(existingTasks).map(key => ({ ...existingTasks[key], id: parseInt(key) })) : [];
+
+    // Sort tasks by their ids
+    tasksArray.sort((a, b) => a.id - b.id);
+
+    // Ensure task ids are sequential starting from 0
+    tasksArray.forEach((task, index) => {
+      if (task.id !== index) {
+        task.id = index;
+        fetch(`${BASE_TASKS_URL}/${index}.json`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(task)
+        });
+      }
+    });
+
+    // Determine the next index
+    const nextIndex = tasksArray.length;
+
+    // Collect new task data
+    const taskData = collectData();
+    if (!taskData) {
+      console.error("No task data to send");
+      return;
+    }
+
+    // Send the new task with the determined index
+    const taskResponse = await fetch(`${BASE_TASKS_URL}/${nextIndex}.json`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(taskData)
+    });
+
+    if (!taskResponse.ok) {
+      throw new Error(`HTTP error! Status: ${taskResponse.status}`);
+    }
+
+    let responseAsJson = await taskResponse.json();
+    console.log('Data saved to Firebase:', responseAsJson);
+  } catch (error) {
+    console.error('Error saving data to Firebase:', error);
+  }
+}
