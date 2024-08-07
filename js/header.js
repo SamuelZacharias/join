aecba2f1eb9f99e9a06ab6b3637e448aa9e59de5
Samuel Zacharias <samuel.zacharias@web.de>
2024-08-07 11:21:14 +0200

    update sidebar d-none

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let includesLoadedHandled = false;
 
   document.addEventListener('includesLoaded', handleIncludesLoaded);
-  
+
   function handleIncludesLoaded() {
     if (!includesLoadedHandled) {
       includesLoadedHandled = true;
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (loggedInUserName) {
       let initials = getInitials(loggedInUserName);
       updateInitialsElement(initials);
-    } 
+    }
   }
 
   function getInitials(userName) {
@@ -45,8 +45,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let initialsElement = document.getElementById('user-profile-initials');
 
     if (logOutElement && !logOutElement.contains(event.target) &&
-        !initialsElement.contains(event.target) &&
-        !event.target.closest('#logOut')) {
+      !initialsElement.contains(event.target) &&
+      !event.target.closest('#logOut')) {
       logOutElement.classList.add('d-none');
     }
   }
@@ -55,6 +55,49 @@ document.addEventListener('DOMContentLoaded', () => {
 function logout() {
   // Wert aus dem lokalen Speicher löschen
   localStorage.removeItem('loggedInUserName');
-  
+
 }
 
+function includeHTML(callback) {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain attribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML(callback);
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+  if (callback) callback();
+}
+
+function hideElements() {
+  if (!localStorage.getItem('loggedInUserName')) {
+    var styleSheet = document.createElement('style');
+    styleSheet.type = 'text/css';
+    styleSheet.innerHTML = `
+          .icon-bar { display: none !important; }
+          #user-profile-initials { display: none !important; }
+      `;
+    document.head.appendChild(styleSheet);
+  }
+}
+
+/* Initial call to load the sidebar and header, then possibly hide the elements */
+includeHTML(hideElements);
